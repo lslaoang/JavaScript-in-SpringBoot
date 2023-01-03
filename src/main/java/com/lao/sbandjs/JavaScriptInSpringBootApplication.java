2dbbe9cd9a69f9298f0ccd8e26cabf0ffe672e74
Laurel Lao Ang <laurellaoang@gmail.com>
2023-01-03 10:38:20 +0800

    Initial commit

---
+++ src/main/java/com/lao/sbandjs/JavaScriptInSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.lao.sbandjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaScriptInSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaScriptInSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lao/sbandjs/JavaScriptInSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lao.sbandjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaScriptInSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
