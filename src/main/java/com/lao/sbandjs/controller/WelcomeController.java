ad03e00931392a0442202f66a5926f5279110c88
Laurel Lao Ang <laurellaoang@gmail.com>
2023-01-03 10:38:55 +0800

    add page controller

---
+++ src/main/java/com/lao/sbandjs/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.lao.sbandjs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+    @RequestMapping("/welcome")
+    public String loginMessage(){
+        return "welcome";
+    }
+}
